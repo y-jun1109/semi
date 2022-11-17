8b3200b276a0b8d0bd59175c919a4c0fdc3d18bf
user <user@DESKTOP-JAMQD40>
2022-11-17 09:31:33 +0900

    02_bs member vo 추가

---
+++ gittest/src/com/git/model/vo/Member.java
@@ -0,0 +1,38 @@
+package com.git.model.vo;
+
+public class Member {
+	private String memberId;
+	private String memberPwd;
+	private String memberName;
+	
+	public Member() {
+		// TODO Auto-generated constructor stub
+	}
+	public Member(String memberId, String memberPwd, String memberName) {
+		super();
+		this.memberId = memberId;
+		this.memberPwd = memberPwd;
+		this.memberName = memberName;
+	}
+	
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPwd() {
+		return memberPwd;
+	}
+	public void setMemberPwd(String memberPwd) {
+		this.memberPwd = memberPwd;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	
+	
+}
